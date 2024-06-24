700c85491a35939cad6e01c2db3a35b40ba0844f
Muhtasir Shafkat <msm.shafkat99@gmail.com>
2024-06-24 15:41:49 +0600

    Fixed anchor tags to ensure proper navigation and functionality

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -17,7 +17,7 @@
       <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
         <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
           <a class="navbar-brand brand-logo" href="{{url('/Dashboard')}}"><i class="mdi mdi-book-open-page-variant text-success"></i> <b class="text-success">eLearning App</b></a>
-          <a class="navbar-brand brand-logo-mini" href="{{url('/')}}"><i class="mdi mdi-book-open-page-variant text-success"></i> <b class="text-success">eLearning</b></a>
+          <a class="navbar-brand brand-logo-mini" href="{{url('/Dashboard')}}"><i class="mdi mdi-book-open-page-variant text-success"></i> <b class="text-success">eLearning</b></a>
         </div>
         <div class="navbar-menu-wrapper d-flex align-items-stretch">
           <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
